a6895108d0b50efccf61f26c863e737c02cd7813
oProgramadorNato <adrianosamuel180@gmail.com>
2025-05-29 16:58:23 -0300

    conceitos-basicos

--- js/index.js
+++ js/index.js
@@ -1,2 +1,42 @@
+//imprimir texto usando script
 document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
-console.log("Oi isso é um console.log")
+//imprimir texto dentro do console
+console.log("Oi isso é um console.log");
+
+
+//formas de sintaxes -> var, const, e let
+/*
+-> var
+Pode ser reatribuída e redeclara múltiplas vezes no mesmo escopo.
+Tem escopo de função, o que pode causar comportamentos inesperados.
+Exemplo:
+        var a = 1;
+        var a = 2;
+        FUNCIONA SEM ERRO
+
+-> let
+Pode ser reatribuída, mas não pode ser redeclarada no mesmo escopo.
+Tem escopo de bloco, o que o torna mais seguro que var.
+
+Exemplo:
+        let idade = 30;
+        let idade = 40; 
+        ESSA SITUAÇÃO OCASIONA EM ERRO POIS 'idade' JÁ FOI DECLARADA
+
+-> const
+Não pode ser reatribuída nem redeclarada.
+Também tem escopo de bloco.
+Pode-se alterar propriedades internas de objetos, mas não trocar a referência.
+
+Exemplo:
+        const idade = 25;
+        idade = 30; 
+        OCASIONA EM ERRO POIS NÃO É POSSIVEL REATRIBUIR UMA CONSTANTE
+*/
+
+
+
+var a=1;
+var b=2;
+var c= a + b;
+console.log(c)
